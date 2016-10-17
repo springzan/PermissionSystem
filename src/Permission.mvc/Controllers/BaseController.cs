014318417fe93ec4cd40a7c43bf8b1e32d4ea1a8
springzan <springzan@hotmail.com>
2016-10-17 16:03:25 +0800

    1.通过BaseController添加是否登录拦截器

---
+++ src/Permission.mvc/Controllers/BaseController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Permission.mvc.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            byte[] result;
+            filterContext.HttpContext.Session.TryGetValue("CurrentUser",out result);
+            if (result == null)
+            {
+                filterContext.Result = new RedirectResult("/Login/Index");
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
--- src/Permission.mvc/Controllers/HomeController.cs
+++ src/Permission.mvc/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Permission.mvc.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         // GET: /<controller>/
         public IActionResult Index()
